9e150ba82fa1d414f511b41017e02f568200c330
Keoma Brun <keoma.brun@inria.fr>
2017-03-15 16:54:36 +0100

    adding simple function to export path events

---
+++ scripts/export_pathevents.py
@@ -0,0 +1,49 @@
+import json
+import influxdb
+import tools
+
+# ======================= Defines =============================================
+
+SITE = "FRA_evalab"
+
+# ======================= Helpers =============================================
+
+def write_to_file(out_file, json_list):
+    # write json to file
+    out_file.write("time,source,dest,direction,\n")
+    for obj in json_list:
+        time = tools.iso_to_epoch(obj["timestamp"])
+
+        out_file.write(
+            str(time)+','+\
+            str(obj["value"]["source"])+','+\
+            str(obj["value"]["dest"])+','+\
+            str(obj["value"]["direction"])+','+\
+            '\n'
+        )
+
+# ======================= Main ================================================
+
+# open output file
+path_create_file = open('../data/event_pathcreate.csv', 'w')
+path_delete_file = open('../data/event_pathdelete.csv', 'w')
+
+# configure influxDB
+influxClient = influxdb.client.InfluxDBClient(
+    host        = 'localhost',
+    port        = '8086',
+    database    = 'realms'
+)
+
+# query influxDB
+query       =   "SELECT * FROM SOL_TYPE_DUST_EVENTPATHCREATE"
+query       +=  " WHERE site='" + SITE + "'"
+query       +=  " GROUP BY mac"
+json_list   = tools.influxdb_to_json(influxClient.query(query).raw)
+write_to_file(path_create_file, json_list)
+
+query       =   "SELECT * FROM SOL_TYPE_DUST_EVENTPATHDELETE"
+query       +=  " WHERE site='" + SITE + "'"
+query       +=  " GROUP BY mac"
+json_list   = tools.influxdb_to_json(influxClient.query(query).raw)
+write_to_file(path_delete_file, json_list)
