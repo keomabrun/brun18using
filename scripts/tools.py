7427259a6e125acf1cbd10f4f1b2c7d0a5b9bfee
Keoma Brun <keoma.brun@inria.fr>
2017-03-28 14:15:06 +0200

    rounding distances

--- scripts/tools.py
+++ scripts/tools.py
@@ -160,4 +160,4 @@ def distance_on_unit_sphere(lat1, long1, lat2, long2):
 
     # Remember to multiply arc by the radius of the earth
     # in your favorite set of units to get length.
-    return arc*6371*1000  # in meters
+    return round(arc*6371*1000, 2)  # in meters
